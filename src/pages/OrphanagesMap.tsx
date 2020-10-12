5e3c444a3f9c3ea5b198702254b68880f7410296
Michael Nascimento <mikanascimento@iMac-de-Michael.local>
2020-10-12 17:23:51 -0300

    Create Orphanages page HTML Markup

--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -1,7 +1,29 @@
 import React from "react";
+import { FiPlus } from "react-icons/fi";
+import { Link } from "react-router-dom";
+
+import MapMarkerImg from "../images/map-marker.svg";
 
 function OrphanagesMap() {
-  return <h1>Hello</h1>;
+  return (
+    <div id="page-map">
+      <aside>
+        <header>
+          <img src={MapMarkerImg} alt="Marca" />
+          <h2>Escolha um orfanato no mapa</h2>
+          <p>Muitas crianças estão esperando a sua visita :)</p>
+        </header>
+        <footer>
+          <strong>Joinville</strong>
+          <span>Santa Catarina</span>
+        </footer>
+      </aside>
+      <div></div>
+      <Link to="" className="create-orphanage">
+        <FiPlus size={32} color="#FFF" />
+      </Link>
+    </div>
+  );
 }
 
 export default OrphanagesMap;
