eafc75cc50360da1b66535026c492a312a25a49b
Michael Nascimento <mikanascimento@iMac-de-Michael.local>
2020-10-12 16:30:48 -0300

    Create Landing HTML Markup

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,25 @@ import React from "react";
 import "./styles/global.css";
 import "./styles/pages/landing.css";
 
+import logo from "./images/logo.svg";
+
 function App() {
   return (
     <div id="page-landing">
-      <h1>Hello World</h1>
+      <div className="content-wrapper">
+        <img src={logo} alt="Happy" />
+        <main>
+          <h1>Leve felicidade para o mundo</h1>
+          <p>Visite orfanatos e mude o dia de muitas crianças.</p>
+        </main>
+        <div className="location">
+          <strong>Joinville</strong>
+          <span>Santa Catarina</span>
+        </div>
+        <a href="" className="enter-app">
+          >
+        </a>
+      </div>
     </div>
   );
 }
