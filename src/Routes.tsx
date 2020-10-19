12667895f4782c9bfc9ae4eade110be73ebdce07
Michael Nascimento <mikanascimento@iMac-de-Michael.local>
2020-10-18 21:57:56 -0300

    Fix Routes.tsx casing problem

---
+++ src/Routes.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import CreateOrphanage from "./pages/CreateOrphanage";
+import Landing from "./pages/Landing";
+import Orphanage from "./pages/Orphanage";
+import OrphanagesMap from "./pages/OrphanagesMap";
+
+function Routes() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Landing} />
+        <Route path="/app" component={OrphanagesMap} />
+        <Route path="/orphanages/create" component={CreateOrphanage} />
+        <Route path="/orphanage/:id" component={Orphanage} />
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+export default Routes;
